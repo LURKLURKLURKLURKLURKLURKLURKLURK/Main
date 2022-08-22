9ef3627a95a66b191d70a7a10683347ad7e32370
LURKLURKLURKLURKLURKLURKLURKLURK <96763530+LURKLURKLURKLURKLURKLURKLURKLURK@users.noreply.github.com>
2022-08-21 20:16:38 -0400

    Update 919KSdK3#LU.lua

--- 919KSdK3#LU.lua
+++ 919KSdK3#LU.lua
@@ -1,13 +1,15 @@
 if getgenv().pie_solutions_env then return end
 local gamename = game:GetService('MarketplaceService'):GetProductInfo(game.PlaceId).Name
 local supported_games = {
-    4588604953, 455366377,
+    4588604953, 455366377, 863266079,
 }
 if not table.find(supported_games,game.PlaceId) and not string.find(gamename,'Criminality') then 
     loadstring(game:HttpGet('https://scripts.luawl.com/11864/Universal.lua'))()
     else 
         if game.PlaceId == 4588604953 then 
             loadstring(game:HttpGet('https://scripts.luawl.com/11829/Criminality.lua'))()
+        elseif game.PlaceId == 863266079 then 
+            loadstring(game:HttpGet('https://scripts.luawl.com/12123/ApocalypseRising2.lua'))()
         elseif game.PlaceId == 455366377 then 
             loadstring(game:HttpGet('https://scripts.luawl.com/11488/TheStreets.lua'))()
         elseif string.find(gamename,'Criminality') then 
