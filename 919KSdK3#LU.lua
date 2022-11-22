3c981f3b86095401e7f0f7bfc752469ddefbbad5
LURKLURKLURKLURKLURKLURKLURKLURK <96763530+LURKLURKLURKLURKLURKLURKLURKLURK@users.noreply.github.com>
2022-11-22 17:57:31 -0500

    Update 919KSdK3#LU.lua

--- 919KSdK3#LU.lua
+++ 919KSdK3#LU.lua
@@ -1,18 +1,39 @@
-if getgenv().pie_solutions_env then return end
-local gamename = game:GetService('MarketplaceService'):GetProductInfo(game.PlaceId).Name
-local supported_games = {
-    4588604953, 455366377, 6407649031,
+-- Loader
+local has_found_game = false
+local plr = game.GetService(game,'Players').LocalPlayer
+local game_name = game.GetService(game, 'MarketplaceService'):GetProductInfo(game.PlaceId).Name
+
+ -- This is for games that do not use custom lobbies/servers
+local supported = {
+    ['4588604953'] = 'https://scripts.luawl.com/11829/Criminality.lua',
+    ['455366377'] = 'https://scripts.luawl.com/13636/TheStreetsv1.0.lua',
+    ['6407649031'] = 'https://scripts.luawl.com/12202/pie.solutions.noscopearcade.lua',
 }
-if not table.find(supported_games,game.PlaceId) and not string.find(gamename,'Criminality') then 
-    loadstring(game:HttpGet('https://scripts.luawl.com/11954/pie.solutions.lua'))()
-    else 
-        if game.PlaceId == 4588604953 then 
-            loadstring(game:HttpGet('https://scripts.luawl.com/11829/Criminality.lua'))()
-        elseif game.PlaceId == 455366377 then 
-            loadstring(game:HttpGet('https://scripts.luawl.com/11488/TheStreets.lua'))()
-        elseif string.find(gamename,'Criminality') then 
-            loadstring(game:HttpGet('https://scripts.luawl.com/11829/Criminality.lua'))()
-        elseif game.PlaceId == 6407649031 then 
-            loadstring(game:HttpGet('https://scripts.luawl.com/12202/pie.solutions.noscopearcade.lua'))()
-        end
+
+ -- This is for games with custom lobbies/servers
+
+local supported_titles = {
+    ['Criminality'] = 'https://scripts.luawl.com/11829/Criminality.lua',
+}
+
+
+for i,v in pairs(supported) do 
+    if tonumber(i) == game.PlaceId or i == game.PlaceId then
+        has_found_game = true
+        loadstring(game:HttpGet(v))()
+        break
+    end 
+end
+
+for i,v in pairs(supported_titles) do 
+    if string.find(game_name,tostring(i)) and not has_found_game then
+        has_found_game = true
+        loadstring(game:HttpGet(v))()
+        break
+    end
+end
+
+
+if not has_found_game then 
+    loadstring(game:HttpGet('https://scripts.luawl.com/13637/universal.v2.lua'))()
 end
